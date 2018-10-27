2c17fc6568e81fc38237b160bb1c052baa1da80d
Aya Kawasumi <sxa.1105bo@gmail.com>
2018-10-27 23:31:09 +0900

    first

---
+++ src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="list-wrapper">
+        <h3>{{ msg }}</h3>
+        <button class="add" @click="add">add</button>
+        <button class="remove" @click="remove">remove</button>
+        <ul class="list">
+            <li v-for="item in list" v-bind:key="item.id">
+               {{ item.task }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    msg: String
+  },
+  data() {
+      return {
+        name: '追加',
+        list: [
+            { id: 1, task: '買う' },
+            { id: 2, task: '売る' }
+        ]
+      }
+  },
+  methods: {
+      add: function() {
+          console.log('add')
+      },
+      remove: () => {
+          console.log('remove')
+      }
+  }
+}
+</script>
