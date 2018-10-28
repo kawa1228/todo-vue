f8450df23a582b98e862a1c6fafc426a7331ea20
Aya Kawasumi <sxa.1105bo@gmail.com>
2018-10-28 17:04:59 +0900

    add Vue.Draggable

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,18 +3,24 @@
         <h2>{{ title }}</h2>
         <input v-model="newTask" placeholder="task">
         <button class="add" @click="add">add</button>
-
-        <ul class="list">
-            <li v-for="(item, index) in list" v-bind:key="item.id">
-               {{ item.task }}
-                <button class="remove" @click="remove(index)">remove</button>
-            </li>
-        </ul>
+        <div class="drag">
+        <draggable :list="list" class="dragArea">
+          <div v-for="(item, index) in list" v-bind:key="item.id">
+            {{ item.task }}
+            <button class="remove" @click="remove(index)">remove</button>
+          </div>
+        </draggable>
+        </div>
     </div>
 </template>
 
 <script>
+import draggable from "vuedraggable";
+
 export default {
+  components: {
+    draggable
+  },
   name: "TodoList",
   props: {
     title: String
@@ -22,7 +28,12 @@ export default {
   data() {
     return {
       newTask: "",
-      list: []
+      list: [
+        { id: 1, task: "hoge" },
+        { id: 2, task: "fuga" },
+        { id: 3, task: "foo" },
+        { id: 4, task: "piyo" }
+      ]
     };
   },
   methods: {
